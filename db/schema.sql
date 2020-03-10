a6fe4e6dd16b07af868b82f670ba0bedc846a8c7
Anamika Panna <anamika2028@gmail.com>
2020-03-10 10:59:45 -0400

    Adding DB schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE database burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(50),
+  devour boolean DEFAULT false,
+  PRIMARY KEY (id)
+);
+
+
+-- Error Code: 1075. Incorrect table definition; there can be only one auto column and it must be defined as a key
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO burgers VALUES (1,'ham burger', true);
+INSERT INTO burgers VALUES (2,'Beef Burgers', true);
+INSERT INTO burgers VALUES (3,'Elk Burgers', true);
+INSERT INTO burgers VALUES (4,'Portobello Mushroom Burgers', true);
+INSERT INTO burgers VALUES (5,'Turkey Burgers', true);
+INSERT INTO burgers VALUES (6,'Veggie Burgers', true);
+INSERT INTO burgers VALUES (7,'Bison Burgers', true);
+INSERT INTO burgers VALUES (8,'Wild Salmon Burgers', true);
+INSERT INTO burgers VALUES (9,'Black Bean Burgers', true);
+
+
+
+-- Beef Burgers.
+-- Elk Burgers.
+-- Portobello Mushroom Burgers.
+-- Turkey Burgers.
+-- Veggie Burgers.
+-- Bison Burgers.
+-- Wild Salmon Burgers.
+-- Black Bean Burgers.
