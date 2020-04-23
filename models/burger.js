06011bebdac56b06364f9b8820f33f991f706db4
Anamika Panna <anamika2028@gmail.com>
2020-04-22 21:37:17 -0400

    updated burger.js with create,update,delete functions

--- models/burger.js
+++ models/burger.js
@@ -6,6 +6,23 @@ var burger = {
         orm.all("burgers", function(res) {
             bd(res);
         });
-    }
-}
+    },
+    // The variables cols and vals are arrays.
+  create: function(cols, vals, bd) {
+    orm.create("burgers", cols, vals, function(res) {
+      bd(res);
+    });
+  },
+  update: function(objColVals, condition, bd) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      bd(res);
+    });
+  },
+  delete: function(condition, bd) {
+    orm.delete("burgers", condition, function(res) {
+      bd(res);
+    });
+  }
+};
+
 module.exports = burger;
