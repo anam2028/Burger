cce1e508ab8400d052e22dc10e36669c7c1052a7
Anamika Panna <anamika2028@gmail.com>
2020-04-22 21:16:31 -0400

    updated burgers_controller with router post,update,delete function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,19 +9,49 @@ var burger = require("../models/burger");
 router.get("/", function(req,res) {
 burger.all(function(data) {
     var hbsObject = {
-        // burgers = data
+        burgers = data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
 });
+});
 
-// router.post("/api/burgers", function(req, res) {
-//     burger.create ( [
-//         eat
-//     ]
+router.post("/api/burgers", function(req, res) {
+    burger.create (["name","eaten"],
+    [req.body.name, req.body.eaten
+      ], function (result){
+    // send back the id of the new quote     
+        res.json({ id:result.insertId });
+    });
+});
 
-//     )
-// })
-})
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update({
+        eaten: req.body.eaten
+    }, condition, function(result){
+        if (result.changedRow == 0) {
+            return res.status(404).end();
+        } else {
+        res.status(200).end();
+    }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+     burger.delete(condition, function (result) {
+            if (result.affectedRow == 0){
+                return res.status(404).end();
+            } else {
+                res.status(200).end()
+            }
+     });
+});
 
 module.exports = router;
