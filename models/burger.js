0ddb72b0cabaa58fa1ea9c0de2af1fd544f6e4b0
Anamika Panna <anamika2028@gmail.com>
2020-04-23 00:26:21 -0400

    updated index.handlebars

--- models/burger.js
+++ models/burger.js
@@ -2,25 +2,25 @@
 var orm = require ("../config/orm");
 
 var burger = {
-    all: function(bd){
+    all: function(cb){
         orm.all("burgers", function(res) {
-            bd(res);
+            cb(res);
         });
     },
     // The variables cols and vals are arrays.
-  create: function(cols, vals, bd) {
+  create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
-      bd(res);
+      cb(res);
     });
   },
-  update: function(objColVals, condition, bd) {
+  update: function(objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function(res) {
-      bd(res);
+      cb(res);
     });
   },
-  delete: function(condition, bd) {
+  delete: function(condition, cb) {
     orm.delete("burgers", condition, function(res) {
-      bd(res);
+      cb(res);
     });
   }
 };
